ef175f45594ff2bc47561e8b5ea2920c1172ebe4
Jacob <jacobhart1227@gmail.com>
2021-11-03 15:58:07 -0600

    Clean up old vue files

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,17 +1,11 @@
 <template>
-<div class="wrapper">
-  <div class="products">
-    <div class="product" v-for="product in products" :key="product.id">
-      <div class="info">
-        <h1>{{product.name}}</h1>
-        <p>{{product.country}}</p>
-      </div>
-      <div class="image">
-        <img :src="'/images/products/'+product.image">
-      </div>
-      <div class="price">
-        <h2>{{product.price}}</h2>
-        <button @click="addToCart(product)" class="auto">Add to Cart</button>
+<div class="row card-deck mt-3">
+  <div class="product" v-for="product in products" :key="product.id">
+    <div class="card" style="width: 14rem;">
+      <div class="card-body">
+        <h5 class="card-title mb-2">{{product.name}}</h5>
+        <h6 class="card-text mb-2">{{product.dark}}</h6>
+        <a href="#" class="card-text text-muted stretched-link">{{product.context.slice(0, 40) + "..."}}</a>
       </div>
     </div>
   </div>
@@ -24,80 +18,5 @@ export default {
   props: {
     products: Array
   },
-  methods: {
-    addToCart(item) {
-      this.$root.$data.cart.push(item)
-    }
-  }
 }
 </script>
-
-<style scoped>
-.wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.products {
-  margin-top: 20px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-}
-
-.product {
-  margin: 10px;
-  margin-top: 50px;
-  width: 200px;
-}
-
-.product img {
-  border: 2px solid #333;
-  height: 250px;
-  width: 200px;
-  object-fit: cover;
-}
-
-.product .image {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 5px;
-}
-
-.info {
-  background: #F2921D;
-  color: #000;
-  padding: 10px 30px;
-  height: 80px;
-}
-
-.info h1 {
-  font-size: 16px;
-}
-
-.info h2 {
-  font-size: 14px;
-}
-
-.info p {
-  margin: 0px;
-  font-size: 10px;
-}
-
-
-.price {
-  display: flex;
-}
-
-button {
-  height: 50px;
-  background: #000;
-  color: white;
-  border: none;
-}
-
-.auto {
-  margin-left: auto;
-}
-</style>
