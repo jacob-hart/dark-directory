eb7cac170b02bc407db304ea6b6ccee67d0959f1
Jacob <jacobhart1227@gmail.com>
2021-11-03 15:36:11 -0600

    Update home display

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
 <div>
-  <div class="wrapper">
-    <div class="search">
-      <form class="pure-form">
-        <i class="fas fa-search"></i><input v-model="searchText" />
-      </form>
+  <form>
+    <div class="form-group">
+      <input class="form-control form-control-lg" v-model="searchText" />
     </div>
-  </div>
+  </form>
   <ProductList :products="products" />
 </div>
 </template>
@@ -31,38 +29,4 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-.wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.search {
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  width: 50%;
-}
-
-form {
-  display: table;
-  width: 100%;
-}
-
-i {
-  display: table-cell;
-  padding-left: 10px;
-  width: 1px;
-}
-
-input {
-  display: table-cell;
-  font-size: 20px;
-  border: none !important;
-  box-shadow: none !important;
-  width: 100%;
-  height: 40px;
-}
-</style>
 
