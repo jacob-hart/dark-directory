334fffc14db90bea408769f65beb94de3cddcf11
Jacob <jacobhart1227@gmail.com>
2021-11-03 16:17:06 -0600

    Update visual design

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="row card-deck mt-3">
   <div class="product" v-for="product in products" :key="product.id">
-    <div class="card" style="width: 14rem;">
+    <div class="card border-success" style="width: 14rem;">
       <div class="card-body">
         <h5 class="card-title mb-2">{{product.name}}</h5>
-        <h6 class="card-text mb-2">{{product.dark}}</h6>
-        <a href="#" class="card-text text-muted stretched-link">{{product.context.slice(0, 40) + "..."}}</a>
+        <h6 class="card-text mb-2 text-success">{{product.dark}}</h6>
+        <a href="#" class="card-text text-muted stretched-link">{{product.context.slice(0, 40).trim() + "..."}}</a>
       </div>
     </div>
   </div>
