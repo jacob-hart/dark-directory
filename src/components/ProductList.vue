c4b764906c9cfb735be919e2ca2f2d728d893e4b
Jacob <jacobhart1227@gmail.com>
2021-11-03 20:07:41 -0600

    Update yes, no, yes but appearance

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,10 +1,11 @@
 <template>
 <div class="row card-deck mt-3">
   <div class="product" v-for="product in products" :key="product.id">
-    <div class="card border-success" style="width: 14rem;">
+    <div class="card border-warning" style="width: 14rem;">
       <div class="card-body">
         <h5 class="card-title mb-2">{{product.name}}</h5>
-        <h6 class="card-text mb-2 text-success">{{product.dark}}</h6>
+        <h6 class="card-text mb-2 text-warning btn btn-outline-warning py-1 px-2">Yes, but...</h6>
+        <br>
         <router-link :to="'/product/' + product.id" class="card-text text-muted stretched-link">{{product.context.slice(0, 40).trim() + "..."}}</router-link>
       </div>
     </div>
