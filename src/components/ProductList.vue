d999ac828fc6930b5de2175f7557254a4c368074
Jacob <jacobhart1227@gmail.com>
2021-11-04 15:47:46 -0600

    Refactor some functionality

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,34 +2,34 @@
 <div class="row d-flex justify-content-center mt-3">
   <div class="product" v-for="product in products" :key="product.id">
     <div v-if="product.dark == 0">
-      <div id="card-0" class="card border-danger bg-dark m-3" style="width: 15rem;">
+      <div id="card-0" class="card border-danger bg-dark m-3" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title text-light mb-2">{{product.name}}</h5>
           <h6 class="card-text mb-2 text-danger btn nohover btn-outline-danger py-1 px-2">No.</h6>
           <br>
-          <p class="card-text text-light mb-0">{{product.context.slice(0, 40).trim() + "..."}}</p>
+          <p class="card-text text-light mb-0">{{truncate(product.context)}}</p>
           <router-link :to="'/product/' + product.id" class="card-text text-muted stretched-link"></router-link>
         </div>
       </div>
     </div>
     <div v-else-if="product.dark == 1">
-      <div id="card-1" class="card border-success bg-dark m-3" style="width: 15rem;">
+      <div id="card-1" class="card border-success bg-dark m-3" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title text-light mb-2">{{product.name}}</h5>
           <h6 class="card-text mb-2 text-success btn nohover btn-outline-success py-1 px-2">Yes!</h6>
           <br>
-          <p class="card-text text-light mb-0">{{product.context.slice(0, 40).trim() + "..."}}</p>
+          <p class="card-text text-light mb-0">{{truncate(product.context)}}</p>
           <router-link :to="'/product/' + product.id" class="card-text text-muted stretched-link"></router-link>
         </div>
       </div>
     </div>
     <div v-else-if="product.dark == 2">
-      <div id="card-2" class="card border-warning bg-dark m-3" style="width: 15rem;">
+      <div id="card-2" class="card border-warning bg-dark m-3" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title text-light mb-2">{{product.name}}</h5>
           <h6 class="card-text text-warning btn nohover btn-outline-warning py-1 px-2">Yes, but...</h6>
           <br>
-          <p class="card-text text-light mb-0">{{product.context.slice(0, 40).trim() + "..."}}</p>
+          <p class="card-text text-light mb-0">{{truncate(product.context)}}</p>
           <router-link :to="'/product/' + product.id" class="card-text text-muted stretched-link"></router-link>
         </div>
       </div>
@@ -44,6 +44,27 @@ export default {
   props: {
     products: Array
   },
+  created() {
+    this.sortProducts()
+  },
+  beforeUpdate() {
+    this.sortProducts()
+  },
+  methods: {
+    truncate(context) {
+      const TRUNCATE_IF_OVER = 55 // contexts over 55 characters will be truncated
+      if (context.length > TRUNCATE_IF_OVER) {
+        return context.slice(0, TRUNCATE_IF_OVER).trim() + "..."
+      }
+      return context
+    },
+    sortProducts() {
+      this.products.sort(function (a, b) {
+        var lookup = { 1: 1, 2: 2, 0: 3 } // Order is: Yes!, Yes, but..., No.
+        return lookup[a.dark] - lookup[b.dark]
+      });
+    }
+  }
 }
 </script>
 
--- src/mock-data.js
+++ src/mock-data.js
@@ -6,18 +6,18 @@
 let mock = [{
     id: 1,
     name: "Google Search (web)",
-    dark: "2",
-    context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    dark: "1",
+    context: "Settings → Dark theme. Will be set automatically based on what it detects from the browser."
   },
   {
     id: 2,
-    name: "Instructure Canvas",
+    name: "Instructure Canvas (web)",
     dark: "0",
     context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
   },
   {
     id: 3,
-    name: "BYU Learning Suite",
+    name: "BYU Learning Suite (web)",
     dark: "1",
     context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
   },
@@ -29,9 +29,63 @@ let mock = [{
   },
   {
     id: 5,
+    name: "iOS",
+    dark: "1",
+    context: "Settings → Display & Brightness → Appearance. Also tints default wallpapers to match."
+  },
+  {
+    id: 6,
+    name: "Windows",
+    dark: "2",
+    context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: 7,
     name: "this website",
     dark: "1",
+    context: "Technically, Bootstrap doesn't support a dark theme. A little CSS coercion goes a long way."
+  },
+  {
+    id: 8,
+    name: "Bootstrap",
+    dark: "0",
     context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: 9,
+    name: "Facebook (web)",
+    dark: "1",
+    context: "Top right dropdown → Display & Accessibility."
+  },
+  {
+    id: 10,
+    name: "BYU app (iOS)",
+    dark: "1",
+    context: "Set automatically based on operating system appearance."
+  },
+  {
+    id: 11,
+    name: "Google Chrome (macOS)",
+    dark: "2",
+    context: "Some elements (New Tab page, Extensions store) are not themed."
+  },
+  {
+    id: 12,
+    name: "Discover (iOS)",
+    dark: "0",
+    context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: 13,
+    name: "Gospel Library (iOS)",
+    dark: "1",
+    context: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+  },
+  {
+    id: 14,
+    name: "Google Drive app (iOS)",
+    dark: "0",
+    context: "Docs, Slides, and Sheets all do, but Drive doesn't. According to Google: \"Dark theme for Drive is only available on Android devices.\""
   },
 ]
 
